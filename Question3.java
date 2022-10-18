ac6b88406159c9de8e50d1ea40e0c4813f795718
Suparv <60036042+Suparv@users.noreply.github.com>
2022-10-18 21:28:14 +0530

    Add files via upload

---
+++ Question3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Question3{
+
+	public static void main(String args[]){
+
+		String str = "abcsjksjakdja"; 
+
+		char []charArr	= str.toCharArray();
+		Arrays.sort(charArr);
+		str = new String(charArr);
+
+		System.out.println(str);
+	}
+}
